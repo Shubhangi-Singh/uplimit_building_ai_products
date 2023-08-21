712623586fe83b9c36adb33edf5780fb1fa3f213
Shubhangi-Singh <47779903+Shubhangi-Singh@users.noreply.github.com>
2023-08-21 20:46:09 +0530

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -4,7 +4,7 @@ import json
 import os
 
 def main():
-    st.title("Newsletter Dashboard")
+    st.title("Tim Ferris Podcast Dashboard")
 
     available_podcast_info = create_dict_from_json_files('.')
 
